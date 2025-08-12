203ece31df51e77120314518384088dfc4bdcd52
MAYANK JAAT <mayankadsens1@gmail.com>
2025-08-12 22:06:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -34,21 +34,21 @@ id_pattern = re.compile(r'^.\d+$')
 
 class Config(object):
     # digital_botz client config
-    API_ID = os.environ.get("API_ID", "")
-    API_HASH = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+    API_ID = os.environ.get("API_ID", "23159366")
+    API_HASH = os.environ.get("API_HASH", "4623dd30dd1303bddb729eb0862262d9")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6598452023:AAGtyH-2z4Di679l5U5ircbgpHskd8cA7OM") 
 
     # premium account string session required 😢 
     STRING_SESSION = os.environ.get("STRING_SESSION", "")
     
     # database config
     DB_NAME = os.environ.get("DB_NAME","Digital_Rename_Bot")     
-    DB_URL = os.environ.get("DB_URL","")
+    DB_URL = os.environ.get("DB_URL","mongodb+srv://WarriorPlayer:WarriorPlayer@warriorplayer.jez7rqs.mongodb.net/?retryWrites=true&w=majority&appName=Warriorplayer")
  
     # other configs
     RKN_PIC = os.environ.get("RKN_PIC", "https://telegra.ph/file/b746aadfe59959eb76f59.jpg")
-    ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '6705898491').split()]
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002123429361"))
+    ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '5222155765').split()]
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002164129051"))
 
     # free upload limit 
     FREE_UPLOAD_LIMIT = 6442450944 # calculation 6*1024*1024*1024=results
@@ -59,9 +59,9 @@ class Config(object):
     
     #force subs
     try:
-        FORCE_SUB = int(os.environ.get("FORCE_SUB", "")) 
+        FORCE_SUB = int(os.environ.get("FORCE_SUB", "-1002023786492")) 
     except:
-        FORCE_SUB = os.environ.get("FORCE_SUB", "Digital_Botz")
+        FORCE_SUB = os.environ.get("FORCE_SUB", "WarriorUnitsBots")
         
     # wes response configuration     
     PORT = int(os.environ.get("PORT", "8080"))
